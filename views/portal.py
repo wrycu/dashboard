69d052355edc48969a6722f85952c69cefb949ae
Wrycu <tim@leetsaber.com>
2025-05-24 15:11:06 -0700

    refactor(sites): update site listing
    * add additional sites
    * refactor to clean up HTML

--- views/portal.py
+++ views/portal.py
@@ -8,4 +8,66 @@ portal = Blueprint('portal', __name__, url_prefix='/')
 
 @portal.route('/')
 def landing():
-    return render_template('portal/base.html')
+    sites = [
+        {
+            'name': 'Mealie',
+            'fav_ico': 'https://mealie.wrycu.com/favicon.ico',
+            'description': 'Recipe book, meal prep helper',
+            'url': 'https://mealie.wrycu.com/',
+        },
+        {
+            'name': 'Paperless-NGX',
+            'fav_ico': 'https://documents.wrycu.com/favicon.ico',
+            'description': 'Document management system',
+            'url': 'https://documents.wrycu.com/',
+        },
+        {
+            'name': 'Pi-hole',
+            'fav_ico': 'https://dns.wrycu.com/admin/img/favicons/favicon.ico',
+            'description': 'DNS, DNS black hole, ad blockingr',
+            'url': 'https://dns.wrycu.com/admin/',
+        },
+        {
+            'name': 'FoundryVTT',
+            'fav_ico': 'https://swrpg.wrycu.com/icons/vtt-512.png',
+            'description': 'TTRPG platform',
+            'url': 'https://swrpg.wrycu.com',
+        },
+        {
+            'name': 'Monica',
+            'fav_ico': 'https://monica.wrycu.com/img/monica.svg',
+            'description': 'Relationship management software',
+            'url': 'https://monica.wrycu.com/',
+        },
+        {
+            'name': 'Immich',
+            'fav_ico': 'https://photos.wrycu.com/favicon.ico',
+            'description': 'Photo management software',
+            'url': 'https://photos.wrycu.com/',
+        },
+        {
+            'name': 'Kerner',
+            'fav_ico': 'https://kener.ing/logo96.png',
+            'description': 'Uptime and status alerting',
+            'url': 'https://status.wrycu.com/',
+        },
+        {
+            'name': 'Actual Budget',
+            'fav_ico': 'https://budget.wrycu.com/favicon.ico',
+            'description': 'Budgeting software',
+            'url': 'https://budget.wrycu.com',
+        },
+        {
+            'name': 'Yamtrack',
+            'fav_ico': 'https://track.wrycu.com/static/favicon/favicon.ico',
+            'description': 'Track movie and TV show watching',
+            'url': 'https://track.wrycu.com',
+        },
+        {
+            'name': 'Jellyfin',
+            'fav_ico': 'https://media.wrycu.com/web/touchicon144.png',
+            'description': 'Watch my media',
+            'url': 'https://media.wrycu.com',
+        },
+    ]
+    return render_template('portal/base.html', sites=sites)
